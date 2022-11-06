d9243e86768976437f6bb490c73466e206f548e0
h-hakim <hhakim@student.42heilbronn.de>
2022-11-06 19:21:00 +0100

    ""

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,56 +6,38 @@
 /*   By: hhakim <hhakim@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 19:23:45 by hhakim            #+#    #+#             */
-/*   Updated: 2022/11/04 20:22:34 by hhakim           ###   ########.fr       */
+/*   Updated: 2022/11/06 17:45:55 by hhakim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_sign(char c)
+int	ft_mvspace(char *s)
 {
-	if (c == '-')
-		return (-1);
-	return (1);
-}
+	int	i;
 
-int	ft_checks(const char *s)
-{
-	while ((*s == '+') || (*s == '-'))
-	{
-		if ((*(s + 1) == '+') || (*(s + 1) == '-'))
-			return (0);
-		return (1);
-		s++;
-	}
-}
-
-int	ft_isspace(char c)
-{
-	if (c == ' ')
-		return (1);
-	if ((c >= '\t') && (c <= '\r'))
-		return (1);
-	return (0);
+	i = 0;
+	while ((s[i] == ' ') || (s[i] >= '\t') && (s[i] <= '\r'))
+		i++;
+	return (i);
 }
 
 int	ft_atoi(const char *s)
 {
-	while ()
-}
+	int	i;
+	int	res;
+	int	sign;
 
-/*
-int atoi( const char *c ) {
-    int value = 0;
-    int sign = 1;
-    if( *c == '+' || *c == '-' ) {
-       if( *c == '-' ) sign = -1;
-       c++;
-    }
-    while ( isdigit( *c ) ) {
-        value *= 10;
-        value += (int) (*c-'0');
-        c++;
-    }
-    return value * sign;
-}*/
+	i = 0;
+	sign = 1;
+	i = ft_mvspace(s);
+	if (s[i] == '-')
+		sign = -1;
+		i ++;
+	while (ft_isdigit(s[i]) && (s[i]))
+	{
+		res = res * 10 + (s[i] - '0');
+		i ++;
+	}
+	return (res * sign);
+}
